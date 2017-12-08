16b6e3381e1dc7becbba37535e22177f16288271
wvfeders <wvfedders@frontier.com>
2017-12-08 15:58:36 -0500

    aborts if not the correct length

--- isbn.rb
+++ isbn.rb
@@ -6,7 +6,8 @@ isbn_array = isbn_number.split("").map(&:to_i)#("") {|s| s.to_i}
 print isbn_array
 puts "Your number has #{isbn_array.count} digits"
 if isbn_array.count != 10 && isbn_array.count != 13
-	print "This number is not of the proper length"
+	abort ("This number is not of the proper length")
+	
 else print "This number is an acceptable length"
 end
 
@@ -16,7 +17,7 @@ number_count.times do
 number  = isbn_array[counter]
 newnumber = newnumber + (number*(counter+1)) #newnumber +(number * counter)
 
-#if counter == 10
+
 print counter, number
 counter = counter +1
 end
