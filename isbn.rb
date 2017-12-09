e5c3aebe03eed582d2f73698562be33e50af6d70
wvfeders <wvfedders@frontier.com>
2017-12-09 12:21:43 -0500

    added comments

--- isbn.rb
+++ isbn.rb
@@ -1,53 +1,55 @@
 
 
-def isbn10(isbn_array, isbn_number)
-counter= 0
-total10 = 0
-10.times do
-number  = isbn_array[counter]
-total10 = total10 + (number*(counter+1))
-counter = counter +1
-end
+def isbn10(isbn_array, isbn_number) #defines a function used to check a 10 digit number
+counter= 0 #establishes a counter
+total10 = 0 #establishes a check number total
+10.times do #initiates a loop through the 10 digit number
+	number  = isbn_array[counter] #pulls number from the isbn array based on its aray location
+	total10 = total10 + (number*(counter+1)) #does multiplication and adds product to total10
+	counter = counter +1 #updates counter
+end #ends do loop
 
-print "The check total is #{total10}.  \n \n"
-if 0 == total10%11
+print "The check total is #{total10}.  \n \n" #prints total10
+if 0 == total10%11 #performs check to see if total10 is evenly divisible by 11 and prints accordingly
+
 	print "#{isbn_number} is a valid 10 digit ISBN number.  \n \n"
 else
 	print "#{isbn_number} is NOT a valid 10 digit ISBN number.  \n \n"	
-end
-end
+end #ends if statement
+end #ends the isbn10 function
 
-def isbn13(isbn_array, isbn_number)
-counter= 0
-total13 = 0
-multiples_array = [1,3,1,3,1,3,1,3,1,3,1,3,1]
-13.times do
-number  = isbn_array[counter]
-multiplier = multiples_array[counter]
-total13 = total13 + (number * multiplier)
-counter += 1
-end
-print "The check total is #{total13}.  \n \n"
-if 0 == total13%10
+
+def isbn13(isbn_array, isbn_number) #defines a function to evaluate a 13 dogot number
+counter= 0 #establishes a counter
+total13 = 0#establishes a check total
+multiples_array = [1,3,1,3,1,3,1,3,1,3,1,3,1] #creates array of values to multiply by the 13 digits
+13.times do #initiates a loop through the 13 digit number
+	number  = isbn_array[counter] #pulls a number from the isbn array based on its array location
+	multiplier = multiples_array[counter] #pulls a number from the multiples array 
+	total13 = total13 + (number * multiplier) #calculates product of digit and multiplier and adds to total13
+	counter += 1 #updates counter
+end #ends loop
+print "The check total is #{total13}.  \n \n" #prints the total13 value
+if 0 == total13%10 #evaluates if total13 is evenly divisible by 10 and prints output acdordingly
 	print "#{isbn_number} is a valid 13 digit ISBN number. \n \n"
 else
 	print "#{isbn_number} is NOT a valid 13 digit ISBN number.  \n \n"	
-end
-end
+end #ends if statement
+end #ends the isbn13 function
 
 
-puts "Type your ISBN number: \n \n" #code only works with lower case
-isbn_number = gets.chomp
-isbn_array = isbn_number.split("").map(&:to_i)#("") {|s| s.to_i}
-#    number_count =  isbn_array.count 
-puts "\nYour number has #{isbn_array.count} digits \n \n"
-if isbn_array.count == 10
-	print "This number is an acceptable length \n \n"
-	isbn10(isbn_array, isbn_number)
-elsif isbn_array.count == 13
-	print "This number is an acceptable length \n \n"
-	isbn13(isbn_array, isbn_number)	
+puts "Type your ISBN number: \n \n" #asks user for numerical input
+isbn_number = gets.chomp #takes in the number
+isbn_array = isbn_number.split("").map(&:to_i)#("") {|s| s.to_i} #parses the number into individual digits and places in the array
+
+puts "\nYour number has #{isbn_array.count} digits \n \n" #outputs how many digits were entered
+if isbn_array.count == 10 #checks to see if 10 digits are in the number
+	print "This number is an acceptable length \n \n" #if 10 digits accepts the number
+	isbn10(isbn_array, isbn_number) #calls the isbn10 function to further evaluate the number
+elsif isbn_array.count == 13 #if number is not 10 digits it checks to see if it is 13 digits
+	print "This number is an acceptable length \n \n" #if 13 digits it accepts the number
+	isbn13(isbn_array, isbn_number)	#calls the isbn13 function to further evaluate the number
 else
-	abort ("This number is not of the proper length  \n \n")
+	abort ("This number is not of the proper length  \n \n") #if the number does not have either 10 or 13 digits, the program exits and prints message
 end	
 
