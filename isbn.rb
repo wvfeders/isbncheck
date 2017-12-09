ef0e22d0ae575a7ca2b652b3633dea8671ab399f
wvfeders <wvfedders@frontier.com>
2017-12-09 11:57:33 -0500

    used two functions and cleaned up code

--- isbn.rb
+++ isbn.rb
@@ -1,58 +1,53 @@
-puts "Type your ISBN number: " #code only works with lower case
-isbn_number = gets.chomp
-isbn_array = isbn_number.split("").map(&:to_i)#("") {|s| s.to_i}
-    number_count=  isbn_array.count 
-
-print isbn_array
-puts "Your number has #{isbn_array.count} digits"
-if isbn_array.count != 10 && isbn_array.count != 13
-	abort ("This number is not of the proper length")
-	
-else print "This number is an acceptable length \n"
-end
 
 
-def isbn10(isbn_array, number_count,isbn_number)
+def isbn10(isbn_array, isbn_number)
 counter= 0
 total10 = 0
-number_count.times do
+10.times do
 number  = isbn_array[counter]
-total10 = total10 + (number*(counter+1)) #newnumber +(number * counter)
-
-
-#puts counter, number, newnumber
+total10 = total10 + (number*(counter+1))
 counter = counter +1
 end
-#end
-print "The check total is #{total10}.  \n"
+
+print "The check total is #{total10}.  \n \n"
 if 0 == total10%11
-	print "#{isbn_number} is a valid 10 digit ISBN number"
+	print "#{isbn_number} is a valid 10 digit ISBN number.  \n \n"
 else
-	print "#{isbn_number} is NOT a valid 10 digit ISBN number"	
+	print "#{isbn_number} is NOT a valid 10 digit ISBN number.  \n \n"	
 end
 end
 
-def isbn13(isbn_array, number_count, isbn_number)
+def isbn13(isbn_array, isbn_number)
 counter= 0
 total13 = 0
 multiples_array = [1,3,1,3,1,3,1,3,1,3,1,3,1]
-number_count.times do
+13.times do
 number  = isbn_array[counter]
 multiplier = multiples_array[counter]
 total13 = total13 + (number * multiplier)
-#puts multiplier, total
 counter += 1
 end
-print "The check total is #{total13}.  \n"
+print "The check total is #{total13}.  \n \n"
 if 0 == total13%10
-	print "#{isbn_number} is a valid 13 digit ISBN number"
+	print "#{isbn_number} is a valid 13 digit ISBN number. \n \n"
 else
-	print "#{isbn_number} is NOT a valid 13 digit ISBN number"	
+	print "#{isbn_number} is NOT a valid 13 digit ISBN number.  \n \n"	
 end
 end
 
+
+puts "Type your ISBN number: \n \n" #code only works with lower case
+isbn_number = gets.chomp
+isbn_array = isbn_number.split("").map(&:to_i)#("") {|s| s.to_i}
+#    number_count =  isbn_array.count 
+puts "\nYour number has #{isbn_array.count} digits \n \n"
 if isbn_array.count == 10
-	isbn10(isbn_array, number_count, isbn_number)
+	print "This number is an acceptable length \n \n"
+	isbn10(isbn_array, isbn_number)
+elsif isbn_array.count == 13
+	print "This number is an acceptable length \n \n"
+	isbn13(isbn_array, isbn_number)	
 else
-	isbn13(isbn_array, number_count, isbn_number)
-end
+	abort ("This number is not of the proper length  \n \n")
+end	
+
